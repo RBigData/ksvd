779e85450f1c6f088533d33063ea2bb1f2f9c9f5
wrathematics <wrathematics@gmail.com>
2018-09-10 21:40:35 -0400

    add skeleton ksvd

---
+++ R/ksvd.r
@@ -0,0 +1,87 @@
+#' Singular Value Decomposition
+#' 
+#' TODO
+#' 
+#' @details
+#' TODO
+#' 
+#' @param x
+#' numeric distributed matrix
+#' @param nu 
+#' number of left singular vectors to return when calculating singular values.
+#' @param nv 
+#' number of right singular vectors to return when calculating singular values.
+#' 
+#' @return 
+#' TODO
+#' 
+#' @export
+ksvd = function(x,  nu = min(m, n), nv = min(m, n))
+{
+  m = nrow(x)
+  n = ncol(x)
+  min_mn = min(m, n)
+  
+  bldim = x@bldim
+  ICTXT = x@ICTXT
+  
+  dim_a = dim(x)
+  ldim_a = pbdBASE::base.numroc(dim=dim_a, bldim=bldim, ICTXT=ICTXT)
+  desca = pbdBASE::base.descinit(dim_a, bldim, ldim_a, ICTXT)
+  
+  if (nu == 0)
+  {
+    jobu = 'N'
+    dim_u = c(1L, 1L)
+  }
+  else
+  {
+    jobu = 'V'
+    dim_u = c(m, min_mn)
+  }
+  
+  ldim_u = pbdBASE::base.numroc(dim=dim_u, bldim=bldim, ICTXT=ICTXT)
+  descu = pbdBASE::base.descinit(dim_u, bldim, ldim_u, ICTXT)
+  
+  if (nv == 0)
+  {
+    jobvt = 'N'
+    dim_vt = c(1L, 1L)
+  }
+  else
+  {
+    jobvt = 'V'
+    vtdim = c(minmn, n)
+  }
+  
+  ldim_vt = pbdBASE::base.numroc(dim=dim_vt, bldim=bldim, ICTXT=ICTXT)
+  descvt = pbdBASE::base.descinit(dim_vt, bldim, ldim_vt, ICTXT)
+  
+  
+  out = rpdgeqsvd(jobu, jobvt, x@Data, desca, descu, descvt)
+  
+  
+  if (nu > 0)
+  {
+    u = new("ddmatrix", Data=out$u, dim=udim, ldim=uldim, bldim=bldim, ICTXT=ICTXT)
+    if (nu < u@dim[2L])
+      u = u[, 1L:nu]
+  }
+  else
+    u = NULL
+  
+  if (nv > 0)
+  {
+    vt = new("ddmatrix", Data=out$vt, dim=vtdim, ldim=vtldim, bldim=bldim, ICTXT=ICTXT)
+    if (nv < vt@dim[1L])
+      vt = vt[1L:nv, ]
+  }
+  else
+    vt = NULL
+  
+  ret = list(d=out$s)
+  ret$u = u
+  ret$vt = vt
+  
+  ret
+}
