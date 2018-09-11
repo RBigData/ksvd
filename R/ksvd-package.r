383c6fea9edaa2962045524849cb746125132dfe
wrathematics <wrathematics@gmail.com>
2018-09-10 20:35:12 -0400

    R boilerplate

---
+++ R/ksvd-package.r
@@ -0,0 +1,17 @@
+#' ksvd
+#' 
+#' TODO
+#' 
+#' @import methods
+#' @import pbdMPI
+#' @import pbdSLAP
+#' @import pbdDMAT
+#' 
+#' @useDynLib ksvd
+#' 
+#' @name ksvd-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+#' @references Programming with Big Data in R Website: \url{https://pbdr.org/}
+#' @keywords Package
+NULL
